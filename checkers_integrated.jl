74e56a2659eed8c38b2887294a7c86b385e165d8
dou10 <denzP2009@yahoo.com>
2018-11-21 10:26:19 -0800

    Add files via upload
    Additional Branch for using the checkers integrated suite

---
+++ checkers_integrated.jl
@@ -0,0 +1,24 @@
+#
+
+julia> using Checkers
+
+#tests the conditional property of Julia
+ julia> @test_formany ntests = 100 maxtests = 100_000 0 < x < 10,  0 < y < 10,  x < y --> x^2 < y^2
+#100 test will be run
+#Passes if 100 tests meet the crierea of holding true. Will fail becuase generating 100 pairs of x,y, some will bound to fail condition
+
+
+#another test the conditional property of Julia
+false. 
+#100,000 tests will performed until 100 passing test
+julia> @test_formany ntests = 100 maxtests = 100_000 0 < x < 10,  0 < y < 10,  x < y --> x^2 < y^2
+#Same as top but will pass because 100000test will be performed.
+
+
+#another test the conditional property of Julia
+
+
+julia> @test_formany ntests = 100 maxtests = 100_000 0 < x < 10,  0 < y < 10,  x < y --> x^2 < y^2
+#Same as top but will pass because 100000test will be performed.
+
+

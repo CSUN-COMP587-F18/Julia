bf39f61a5a4b747de5098473e48b7328f2f6ef17
dou10 <denzP2009@yahoo.com>
2018-12-16 01:03:35 -0800

    Update codeGenrator_test.jl

--- codeGenrator_test.jl
+++ codeGenrator_test.jl
@@ -15,14 +15,18 @@ end
 	x = MyNumber(pi)
 
 	@test cos(x) == MyNumber(-1.0)
-
+	@time cos(x)
+	# 0.025440 seconds (122.20 k allocations: 6.844 MiB, 11.69% gc time)
 	#sin(x) should be 1.2246467991473532e-16
 	@test sin(x) == MyNumber(1.2246467991473532e-16)
-
 	@test tan(x) == MyNumber(-1.2246467991473532e-16)
 	@test log(x) == MyNumber(1.1447298858494002)
+	@time log(x)
 	@test exp(x) == MyNumber(23.140692632779267)
-
+	@test csc(x) == MyNumber(8.165619676597685e15)
+	@time csc(x) 
+	@test sinpi(x) == MyNumber(-0.43030121700009194)
+	@test cbrt(x) == MyNumber(1.4645918875615231)
 	
 #test using an integer = 1
 struct MyNumber
@@ -33,6 +37,7 @@ end
 	@test sin(y) == MyNumber2(0.8414709848078965)
 	@test tan(y) == MyNumber2(1.5574077246549023)
 	@test log(y) == MyNumber2(0.0)
+	@time log(y)
 	@test exp(y) == MyNumber2(2.718281828459045)
 
 #test

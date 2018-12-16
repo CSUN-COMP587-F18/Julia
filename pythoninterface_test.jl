518cbcca0db06dd96389912cb272ba2002a0e17c
dou10 <denzP2009@yahoo.com>
2018-12-15 18:54:23 -0800

    python interface unit test
    Basic python interface unit test

---
+++ pythoninterface_test.jl
@@ -0,0 +1,92 @@
+#using python interface for Julia
+using PyCall
+
+#to test math functions and data strucrts from Python to Julia
+@pyimport math
+
+#using @test for unit testing
+
+#test using Python Lists in Julia
+let x = PyObject(test[1,2])
+
+	@test pop!(x) == 2
+	@test collect(x) == 1
+	@test collect(push!(x, 2)) == [1,2]
+	@test collect(append!(x, 3,4,5)) == [1,2,3,4,5]
+	@test collect(prepend!(x,0)) == [0,1,2,3,4,5]
+	@test collect(pop!(x)) == [0,1,2,3,4]
+	@test collect(append!(x,x)) == [0,1,2,3,4,0,1,2,3,4]
+
+#quick test for strings
+let y = PyObject(test2[one,two])
+
+	@test collect(y) == [one,two]
+	@test collect(push!(y,three)) == [one,two,three]
+	@test collect(pop!(y)) == [one,two]
+
+	#this test above fails for some reason, probably wrong syntax
+
+#basic matrix tests
+
+	@test [10,20] + [10,20] == [30,30]
+	@test [10,20] - [10,20] == [0,0]
+
+
+#math sin functions
+
+@test math.sin(math.pi / 4 - sin(pi / 4)) == 0
+@test math.pi - pi == 0
+@test math.pi/2 - pi/2 == 0
+
+@test math.pow(2,2) == 4
+
+#using Python syntax to run basic function
+
+let x = 5
+py"""
+def testFunction(x):
+	return 2 * x
+"""
+	@test py"testFunction"(5) == 10
+	@test py"testFunction"(0) == 0
+
+#passing Julia variable in python function
+
+let x = 2
+py"""
+def testFunction2(x):
+	return x * $x
+"""
+
+	@test py"testFunction2"(5) == 10
+	# 2 * x
+	@test py"2 * $x" == 4
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
